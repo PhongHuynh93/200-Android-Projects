7c0cb5ceff4ed28b760294ec96355dee0f2f7aac
Phong <p7157568@gmail.com>
2016-08-25 22:33:34 +0700

    feat - get image url from galery

--- AddPhoto/app/src/main/java/com/edimaudo/addphoto/MainActivity.java
+++ AddPhoto/app/src/main/java/com/edimaudo/addphoto/MainActivity.java
@@ -17,65 +17,74 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-  private ImageView photo;
-  private static int RESULT_LOAD_IMAGE = 1;
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    photo = (ImageView) findViewById(R.id.photo);
-  }
+    private ImageView photo;
+    private static int RESULT_LOAD_IMAGE = 1;
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    MenuInflater inflater = getMenuInflater();
-    inflater.inflate(R.menu.main_menu, menu);
-    return super.onCreateOptionsMenu(menu);
-
-  }
-
-  public boolean onOptionsItemSelected(MenuItem item) {
-    switch (item.getItemId()) {
-      case R.id.action_add:
-        isStoragePermissionGranted();
-        Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(i, RESULT_LOAD_IMAGE);
-        break;
-      default:
-        break;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        photo = (ImageView) findViewById(R.id.photo);
     }
 
-    return true;
-  }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
 
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-      Uri selectedImage = data.getData();
-      String[] filePathColumn = { MediaStore.Images.Media.DATA };
-      Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
-      cursor.moveToFirst();
-      int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-      String picturePath = cursor.getString(columnIndex);
-      cursor.close();
-      photo.setImageBitmap(BitmapFactory.decodeFile(picturePath));
     }
-  }
 
-  public boolean isStoragePermissionGranted() {
-    if (Build.VERSION.SDK_INT >= 23) {
-      if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-              == PackageManager.PERMISSION_GRANTED) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                isStoragePermissionGranted();
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+                break;
+            default:
+                break;
+        }
+
         return true;
-      } else {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-        return false;
-      }
-    } else {
-      return true;
     }
-  }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            // todo - get the path string of an image which has store in galery
+            Uri selectedImage = data.getData();
+
+            // todo - filePathColumn chỉ cái cột mà ta lấy data, cursor chỉ lấy có 1 cột
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            // todo - lấy string từ cột là là DATA
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            // TODO: 8/25/16 open image by that path
+            photo.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
+    }
+
+    // TODO: 8/25/16 - check permission in android 23
+    public boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
 
 
 }
